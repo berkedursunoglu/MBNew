8ee4e28c03ec1e9e6db76f34464811974798c786
Berke Dursunoglu <berkedursunoglu@gmail.com>
2022-07-19 17:32:23 +0300

    login

--- app/src/main/java/com/berkedursunoglu/mbnews/API/NewsClient.kt
+++ app/src/main/java/com/berkedursunoglu/mbnews/API/NewsClient.kt
@@ -7,7 +7,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 abstract class RetrofitInstance {
 
     companion object{
+
         private val retrofit = Retrofit.Builder().addConverterFactory(GsonConverterFactory.create()).baseUrl(Constants.baseUrl).build()
+
         val retrofitInstance: NewsAPI by lazy {
             retrofit.create(NewsAPI::class.java)
         }
